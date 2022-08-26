ba0cac0ec6d80d25b19d9856f4bff856ffc55d55
rubensrvsc <rubensspfc100@gmail.com>
2022-08-26 19:17:22 -0300

    refactor: methods of admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,26 @@
 class AdminController < ApplicationController
+    # before_action :authenticate_user!, only: %[matriculate_student]
+    include Container::Import[:repo, :create_matriculation]
 
     def index
         
     end
 
     def inscripts
-        @students_inscripts = Student.where(status: false)
+        @students_inscripts = repo.students_inscripts
     end
 
     def matriculates
-        @students_matriculates = Student.where(status: true)
+        @students_matriculates = repo.students_matriculates
+    end
+
+    def student_details
+        @student_details = repo.details_student(params[:id])
     end
 
     def matriculate_student
-        student = Student.find_by(id: params[:id])
-        student.status = true
-        student.save
+        create_matriculation.call(params[:id])
+
+        redirect_to action: "index" and return
     end
 end
