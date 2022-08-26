45c6621df51375346a4cfa8ba8e4a1ee8bfa4b74
rubensrvsc <rubensspfc100@gmail.com>
2022-08-26 19:16:35 -0300

    feat: add create inscription

--- app/domain/business/create_inscription.rb
+++ app/domain/business/create_inscription.rb
@@ -4,16 +4,30 @@ module Business
     class CreateInscription
         include Dry::Monads[:result, :do]
         
-        def call
-            
+        def call(params)
+            params_sanitize = yield sanitize_params(params)
+            params_validated = yield validate_attrivutes(params)
+            inscription = yield save_inscription(params)
+
+            Success(inscription)
         end
 
-        def format_date
-            
+        def sanitize_params(params)
+            Success(params.permit(:student_name,:email,:cpf,:birth,:shift,:serie,:city,:state,:responsible_name,:responsible_email,:responsible_phone))
         end
 
-        def save_inscription
-            
+        def validate_attributes(params)
+            student = Student.find_by(cpf: params[:email])
+
+            if student.present?
+                return Failure('Estudante já cadastrado')
+            end
+
+            Success(params)
+        end
+
+        def save_inscription(params)
+            Success(Student.create(params))
         end
     end
 end
