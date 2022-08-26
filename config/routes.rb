bf4aebd426bf2efcb673c445fbab005cc861c777
rubensrvsc <rubensspfc100@gmail.com>
2022-08-26 19:19:15 -0300

    feat: add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "student#register_student"
   post "create_student", to: "student#create"
+  get "index", to: "admin#index"
+  get "inscripts", to: "admin#inscripts"
+  get "matriculates", to: "admin#matriculates"
+  get "details/:id", to: 'admin#student_details'
+  post "matricular_aluno", to: "admin#matriculate_student"
 end
