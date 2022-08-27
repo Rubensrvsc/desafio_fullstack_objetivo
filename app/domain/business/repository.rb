368cab48e487e728576cd9c43598970aa9806d0d
rubensrvsc <rubensspfc100@gmail.com>
2022-08-27 19:53:57 -0300

    refactor: update repository

--- app/domain/business/repository.rb
+++ app/domain/business/repository.rb
@@ -1,16 +1,20 @@
 module Business
     class Repository
         
-        def students_inscripts
-            Student.where(status: false)
+        def students_inscripts(page)
+            Student.where(status: false).page(page).per(3)
         end
 
-        def students_matriculates
-            Student.where(status: true)
+        def students_matriculates(page)
+            Student.where(status: true).page(page).per(3)
         end
 
         def details_student(id_student)
             Student.find_by(id: id_student)
+        end
+
+        def csv_inscripts
+            Student.where(status: false).generate_csv
         end
     end
 end
