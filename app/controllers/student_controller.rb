f820d93c535701bf97d3c7f1a94e6dfe93ab58e2
rubensrvsc <rubensspfc100@gmail.com>
2022-08-27 19:02:37 -0300

    refactor: creation of inscript

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -6,6 +6,17 @@ class StudentController < ApplicationController
     end
 
     def create
-        create_inscription.call(params)
+        result = create_inscription.call(set_params)
+        redirect_to action: 'confirmation_register', result: result.success? ? result.value! : result.failure
+    end
+
+    def confirmation_register
+        @result = params[:result]
+    end
+
+    private
+
+      def set_params
+        params.permit(:student_name,:email,:cpf,:birth,:shift,:serie,:city,:state,:responsible_name,:responsible_email,:responsible_phone)
     end
 end
