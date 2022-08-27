d4c5d1768c6f78986ba9d064d2751826664dbd91
rubensrvsc <rubensspfc100@gmail.com>
2022-08-27 19:57:56 -0300

    model: update student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < ApplicationRecord
     attribute :status, :boolean, default: false
+
+    def self.generate_csv
+        CSV.generate("\uFEFF", col_sep: ';') do |csv|
+            csv << column_names
+            all.each do |student|
+                csv << student.attributes.values_at(*column_names)
+            end
+        end
+    end
 end
