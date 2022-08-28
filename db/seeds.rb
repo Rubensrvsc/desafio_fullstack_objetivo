1aef95ef18e9564313cdc3ae313c3ea928ecf8e8
rubensrvsc <rubensspfc100@gmail.com>
2022-08-28 11:34:44 -0300

    feat: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'janairo@gmail.com', password: 'janairo123', password_confirmation: 'janairo123')
